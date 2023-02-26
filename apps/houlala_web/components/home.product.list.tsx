7a3a4489c87634695c28d018e130b5c9546815f6
pierre Edimo <pedimonkoe@yahoo.com>
2023-02-26 22:55:11 +0100

    changer le titre

--- apps/houlala_web/components/home.product.list.tsx
+++ apps/houlala_web/components/home.product.list.tsx
@@ -2,12 +2,12 @@ import ProductGrid from "./product.grid";
 
 
 export function HomeProductList() {
-    const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
+  const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
 
-    return (
-        <div>
-            <h2>Fruits & Legumes</h2>
-            <ProductGrid url={`${PRODUCT_URL}`}/>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Quelques Fruits & Legumes</h2>
+      <ProductGrid url={`${PRODUCT_URL}/random/category/638d214e9e725bd8c2dca355?size=8`} />
+    </div>
+  );
 }
