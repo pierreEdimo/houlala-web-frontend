0bfdf1e7e569db2eabea660a81729633c3e95baa
pierre Edimo <pedimonkoe@yahoo.com>
2023-02-26 22:56:08 +0100

    ajouter le pathname

--- apps/houlala_web/components/bottom.bar.tsx
+++ apps/houlala_web/components/bottom.bar.tsx
@@ -1,41 +1,50 @@
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/bottom.bar.module.scss";
-import {NestedLayout} from "./nested.layout";
+import { NestedLayout } from "./nested.layout";
 import Image from "next/image";
 import house from "../public/images/house.png";
 import category from "../public/images/category.png";
 import cart from "../public/images/cart.png";
 import user from "../public/images/user.png";
-import {useRecoilState} from "recoil";
+import { useRecoilState } from "recoil";
 import UserModal from "../state/user.modal";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 
 const BottomBar = () => {
-    const [isOpen, setIsOpen] = useRecoilState(UserModal);
-    const openModal = () => setIsOpen(true);
+  const [isOpen, setIsOpen] = useRecoilState(UserModal);
+  const openModal = () => setIsOpen(true);
+  const router = useRouter();
 
-    return (
-        <div className={styles.bottomBar}>
-            <NestedLayout>
-                <div className={styles.bottomBarContent}>
-                    <Link href="/">
-                        <Image src={house} width={25} height={25} alt="house-image"/>
-                    </Link>
-                    <Link href="/discover">
-                        <Image src={category} width={25} height={25} alt="category-image"/>
-                    </Link>
-                    <Link href="/cart">
-                        <Image src={cart} width={25} height={25} alt="corbeille-image"/>
-                    </Link>
-                    <div onClick={openModal}>
-                        <Image src={user} width={25} height={25} alt="user-image"/>
-                    </div>
-                </div>
-            </NestedLayout>
+  return (
+    <div className={styles.bottomBar}>
+      <NestedLayout>
+        <div className={styles.bottomBarContent}>
+          <Link href="/">
+            <div className={router.pathname === "/" ? styles.activeIcon : styles.iconContainer}>
+              <Image src={house} width={25} height={25} alt="house-image" />
+            </div>
+          </Link>
+          <Link href="/discover">
+            <div className={router.pathname === "/discover" ? styles.activeIcon : styles.iconContainer}>
+              <Image src={category} width={25} height={25} alt="category-image" />
+            </div>
+          </Link>
+          <Link href="/cart">
+            <div className={router.pathname === "/cart" ? styles.activeIcon : styles.iconContainer}>
+              <Image src={cart} width={25} height={25} alt="corbeille-image" />
+            </div>
+          </Link>
+          <div onClick={openModal}>
+            <div className={styles.iconContainer}>
+              <Image src={user} width={25} height={25} alt="user-image" />
+            </div>
+          </div>
         </div>
-    )
-}
+      </NestedLayout>
+    </div>
+  );
+};
 
 export default BottomBar;
