08b75bce1a6ae7ba3bfab939ba64896ab953d6f9
pierre Edimo <pedimonkoe@yahoo.com>
2023-02-26 22:55:28 +0100

    ajouter le titre et l'url

--- apps/houlala_web/components/button.container.tsx
+++ apps/houlala_web/components/button.container.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Image, {StaticImageData} from "next/image";
 import styles from "../styles/button.module.scss";
+import { useRouter } from "next/router";
 
 
 type ButtonContainerProps = {
-    imageSrc: StaticImageData,
-    onClick?: () => void
+    imageSrc: StaticImageData;
+    onClick?: () => void;
+    title?: string;
+    url?: string;
 }
 
-const ButtonContainer: React.FC<ButtonContainerProps> = ({imageSrc, onClick}: {
-    imageSrc: StaticImageData,
-    onClick?: () => void
-}) => {
+const ButtonContainer: React.FC<ButtonContainerProps> = ({imageSrc, onClick, title, url}) => {
+    const router = useRouter();
+
     return (
         <>
-            <div onClick={onClick}
+            <div style={{background: router.pathname == `${url}` ? "orange": "" }} title={title} onClick={onClick}
                  className={styles.roundedButtonContainer}>
                 <Image src={imageSrc} alt={"icon-image"} width={25} height={25}/>
             </div>
