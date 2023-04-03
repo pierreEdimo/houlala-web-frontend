518c0272ac30f59838886b199ee3fb7efa88ff17
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:37:33 +0200

    creation de la page Dashboard

---
+++ apps/houlala_sell/pages/dashboard/[uid].tsx
@@ -0,0 +1,156 @@
+import styles from "../../styles/Home.module.scss";
+import { BorderedCard, Container, Row, IconImage } from "ui";
+import { useLocation } from "../../hooks/location.hooks";
+import products from "../../public/images/box.png";
+import Image from "next/image";
+import checklst from "../../public/images/checklist.png";
+import checkout from "../../public/images/check-out.png";
+import cancel from "../../public/images/cancel.png";
+import OrderListCard from "../../components/order-list/order.list.card";
+import ProductListCard from "../../components/product-list/product.list.card";
+import NestedLayout from "../../components/nested-layout/nested.layout";
+import InfoProductListCard from "../../components/product-list/info.product.list.card";
+import { useEffect } from "react";
+
+
+const Dashboard = () => {
+    const {
+        location,
+        error,
+        isLoading
+    } = useLocation(`${process.env.NEXT_PUBLIC_LOCATION_URL}/users/27d54546-5c23-40b3-be65-6701f89e4d9b`);
+    const ORDER_URL = process.env.NEXT_PUBLIC_ORDER_URL;
+    const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
+
+    useEffect(() => {
+
+    });
+
+
+    if (isLoading) {
+        return (
+            <div>
+                ....Loading
+            </div>
+            );
+    }
+
+    if (error) {
+        return (
+            <div>
+                ...Errors
+            </div>
+            );
+    }
+
+    return (
+        <NestedLayout>
+            <div className={styles.container}>
+                <Container>
+                    <h2>Dashboard</h2>
+                    <br />
+                    <div className={styles.cardContainer}>
+                        <BorderedCard>
+                            <div className={styles.boxContainer}>
+                                <div style={{ padding: "1rem" }}>
+                                    <h4> Produits</h4>
+                                    <p>{location?.productTotalCount}</p>
+                                </div>
+                                <div style={{ display: "flex", justifyContent: "space-between", padding: "1rem" }}>
+                                    <div></div>
+                                    <Image src={products}
+                                        style={{ float: "right" }}
+                                        alt={"product-image"}
+                                        width={35}
+                                        height={35} />
+                                </div>
+                            </div>
+                        </BorderedCard>
+                        <BorderedCard>
+                            <div className={styles.boxContainer}>
+                                <div style={{ padding: "1rem" }}>
+                                    <h4>Commandes</h4>
+                                    <p>{location?.orderTotalCount}</p>
+                                </div>
+                                <div style={{ display: "flex", justifyContent: "space-between", padding: "1rem" }}>
+                                    <div></div>
+                                    <Image src={checklst}
+                                        style={{ float: "right" }}
+                                        alt={"product-image"}
+                                        width={35}
+                                        height={35} />
+                                </div>
+                            </div>
+                        </BorderedCard>
+                        <BorderedCard>
+                            <div className={styles.boxContainer}>
+                                <div style={{ padding: "1rem" }}>
+                                    <h4>Ventes</h4>
+                                    <p>{location?.orderSoldCount}</p>
+                                </div>
+                                <div style={{ display: "flex", justifyContent: "space-between", padding: "1rem" }}>
+                                    <div></div>
+                                    <Image src={checkout}
+                                        style={{ float: "right" }}
+                                        alt={"product-image"}
+                                        width={35}
+                                        height={35} />
+                                </div>
+                            </div>
+                        </BorderedCard>
+                        <BorderedCard>
+                            <div className={styles.boxContainer}>
+                                <div style={{ padding: "1rem" }}>
+                                    <h4>Annulations</h4>
+                                    <p>{location?.orderCanceledCount}</p>
+                                </div>
+                                <div style={{ display: "flex", justifyContent: "space-between", padding: "1rem" }}>
+                                    <div></div>
+                                    <Image src={cancel}
+                                        style={{ float: "right" }}
+                                        alt={"product-image"}
+                                        width={35}
+                                        height={35} />
+                                </div>
+                            </div>
+                        </BorderedCard>
+                    </div>
+                    <br />
+                    <Row>
+                        <div className={styles.leftCol}>
+                            <OrderListCard
+                                url={`${ORDER_URL}/location/${location?.uniqueIdentifier}`}
+                                title={"Les commandes du jour"}
+                                position={"home"}
+                                locationId={`${location?.uniqueIdentifier}`}
+                            />
+                            <ProductListCard title={"Appercu des produits"}
+                                url={`${PRODUCT_URL}/location/${location?.uniqueIdentifier}?limit=25`}
+                                locationId={location?.uniqueIdentifier}
+                                position={"home"}
+                            />
+                            <br />
+                            <OrderListCard
+                                url={`${ORDER_URL}/location/filter?locationId=${location?.uniqueIdentifier}`}
+                                title={"Commandes recentes"}
+                                position={"home"}
+                                locationId={`${location?.uniqueIdentifier}`}
+                            />
+                        </div>
+                        <div className={styles.rightCol}>
+                            <InfoProductListCard
+                                url={`${PRODUCT_URL}/top/${location?.uniqueIdentifier}`}
+                                title={"Top Produits"} />
+                            <br />
+                            <InfoProductListCard
+                                url={`${PRODUCT_URL}/stock/${location?.uniqueIdentifier}`}
+                                title={"Bientot en rupture de stock"} />
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+        </NestedLayout>
+        );
+}
+
+export default Dashboard; 
