510b941d84797abe1ea1e7494d609041abde9eda
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:41:31 +0200

    ajout du nestedlayout dans la page de stock

--- apps/houlala_sell/pages/stock/[uid].tsx
+++ apps/houlala_sell/pages/stock/[uid].tsx
@@ -3,33 +3,33 @@ import { BorderedCard, Container, Row } from "ui";
 import style from "../../styles/Home.module.scss";
 import ProductListCard from "../../components/product-list/product.list.card";
 import { useRouter } from "next/router";
+import NestedLayout from "../../components/nested-layout/nested.layout";
+import { useRecoilState } from "recoil";
+import AuthState from "../../state/auth.state";
+import { useEffect } from "react";
 
 const Stock: NextPage = () => {
-  const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
-  const router = useRouter();
-  const {uid} = router.query;
-  return (
-    <>
-      <Container>
-        <h2>Stock</h2>
-        <br />
-      </Container>
-      <Row>
-        <div className={style.leftCol}>
-          <ProductListCard
-            title={"Produits"}
-            url={`${PRODUCT_URL}/location/${uid}?limit=0`} />
-        </div>
-        <div className={style.rightCol}>
-          <BorderedCard style={{ padding: "1rem" }}>
-            <div className={style.cardHeader}>
-              <h3>Appercu</h3>
+    const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
+    const router = useRouter();
+    const { uid } = router.query;
+    const [isLoggedIn] = useRecoilState(AuthState);
+
+    useEffect(() => {
+    });
+
+    return (
+        <NestedLayout>
+            <Container>
+                <h2>Stock</h2>
+                <br />
+            </Container>
+            <div className={style.mainContainer}>
+                <ProductListCard
+                    title={"Produits"}
+                    url={`${PRODUCT_URL}/location/${uid}?limit=0`} />
             </div>
-          </BorderedCard>
-        </div>
-      </Row>
-    </>
-  );
+        </NestedLayout>
+    );
 };
 
 export default Stock;
