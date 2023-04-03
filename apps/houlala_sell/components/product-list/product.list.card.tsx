9b098f5d8016e5cd6a8ebc5dc15aa84f6ad583fa
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:31:38 +0200

    initialisez le useRouter

--- apps/houlala_sell/components/product-list/product.list.card.tsx
+++ apps/houlala_sell/components/product-list/product.list.card.tsx
@@ -4,95 +4,92 @@ import { Avatar, BorderedCard } from "ui";
 import style from "./product.list.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 type Props = {
-  title: string;
-  url: string;
-  position?: string;
-  locationId?: string;
+    title: string;
+    url: string;
+    position?: string;
+    locationId?: string;
 }
 const ProductListCard: React.FC<Props> = ({ url, title, position, locationId }) => {
-  const { products, error, isLoading } = useProductList(url);
+    const { products, error, isLoading } = useProductList(url);
+    const router = useRouter();
 
-  if (isLoading) {
-    return (
-      <div>
-        ...Loading
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div>
-        ...Error
-      </div>
-    );
-  }
-
-  return (
-    <>
-      {
-        products?.length! < 1 ?
-          <div></div> :
-          <BorderedCard style={{ padding: "1rem" }}>
-            <div className={style.cardHeader}>
-              <h3>{title}</h3>
-              {
-                position === "home" ?
-                  <Link href={`/stock/${locationId}`}>
-                    <p>Voir tout</p>
-                  </Link>
-                  : <div></div>
-              }
+    if (isLoading) {
+        return (
+            <div>
+                ...Loading
             </div>
-            <br />
-            <table className={style.productTable} style={{ width: "100%" }}>
-              <tr>
-                <th>Nom du produit</th>
-                <th>Prix d'achat</th>
-                <th>Prix de vente</th>
-                <th>Quantite</th>
-              </tr>
-              {
-                products?.map((product) => (
-                  <tr className={style.productRow} key={product._id}>
-                    <td>
-                      <div style={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "1rem"
-                      }}>
-                        <Avatar style={{
-                          width: "40px",
-                          height: "40px",
-                          borderRadius: "5px"
-                        }}>
-                          <Image fill
-                                 style={{ objectFit: "cover" }}
-                                 src={product.imageUrl}
-                                 alt={"product-image"} />
-                        </Avatar>
-                        <p style={{ fontSize: "13.5px" }}>{product.name}</p>
-                      </div>
-                    </td>
-                    <td>
-                      <p className={style.textContent}>{product.buyingPrice}</p>
-                    </td>
-                    <td>
-                      <p className={style.textContent}>{product.sellingPrice}</p>
-                    </td>
-                    <td>
-                      <p className={style.textContent}>{product.quantity}</p>
-                    </td>
-                  </tr>
-                ))
-              }
-            </table>
-          </BorderedCard>
-      }
-    </>
-  );
+        );
+    }
+
+    if (error) {
+        return (
+            <div>
+                ...Error
+            </div>
+        );
+    }
+
+    return (
+        <>
+            {
+                products?.length! < 1 ?
+                    <div></div> :
+                    <BorderedCard style={{ padding: "1rem" }}>
+                        <div className={style.cardHeader}>
+                            <h3>{title}</h3>
+                            {
+                                position === "home" ?
+                                    <Link href={`/stock/${locationId}`}>
+                                        <p>Voir tout</p>
+                                    </Link>
+                                    : <div></div>
+                            }
+                        </div>
+                        <br />
+                        <table className={style.productTable}>
+                            <tbody>
+                                <tr>
+                                    <th className={style.tableTitle}>Image</th>
+                                    <th className={style.tableTitle}>Nom du produit</th>
+                                    <th className={style.tableTitle}>Prix de vente</th>
+                                    <th className={style.tableTitle}>Quantite</th>
+                                </tr>
+                            </tbody>
+                            {
+                                products?.map((product) => (
+                                    <tr onClick={() => router.push(`/product-detail/${product._id}`)} className={style.productRow} key={product._id}>
+                                        <td>
+                                            <Avatar style={{
+                                                width: "30px",
+                                                height: "30px",
+                                                borderRadius: "0.3rem"
+                                            }}>
+                                                <Image fill
+                                                    style={{ objectFit: "cover" }}
+                                                    src={product.imageUrl}
+                                                    alt={"product-image"} />
+                                            </Avatar>
+                                        </td>
+                                        <td>
+                                            <p className={style.textContent}>{product.name}</p>
+                                        </td>
+                                        <td>
+                                            <p className={style.textContent}>{product.sellingPrice} XAF</p>
+                                        </td>
+                                        <td>
+                                            <p className={style.textContent}>{product.quantity}</p>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </table>
+                    </BorderedCard>
+            }
+        </>
+    );
 };
 
 export default ProductListCard; 
