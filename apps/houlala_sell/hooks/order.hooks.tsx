2d2d0309c46d7e2516ee5ed74b8197157f2d8cac
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:32:18 +0200

    ajouter methose pour fait une request vers la base de donnees

--- apps/houlala_sell/hooks/order.hooks.tsx
+++ apps/houlala_sell/hooks/order.hooks.tsx
@@ -3,13 +3,23 @@ import useSWR from "swr";
 import { Order } from "../types/order";
 
 function useOrderList(orderUrl: string) {
-  const fetcher = async (url: string) => await axios.get<Order[]>(url).then((res) => res.data);
-  const { data, error, isLoading } = useSWR<Order[]>(orderUrl, fetcher);
-  return {
-    orders: data,
-    isLoading,
-    error
-  };
+    const fetcher = async (url: string) => await axios.get<Order[]>(url).then((res) => res.data);
+    const { data, error, isLoading } = useSWR<Order[]>(orderUrl, fetcher);
+    return {
+        orders: data,
+        isLoading,
+        error
+    };
 }
 
-export { useOrderList };
+function useOrder(orderUrl: string) {
+    const fetcher = async (url: string) => await axios.get<Order>(url).then((res) => res.data);
+    const { data, error, isLoading } = useSWR<Order>(orderUrl, fetcher);
+    return {
+        order: data,
+        isLoading,
+        error
+    };
+}
+
+export { useOrderList, useOrder };
