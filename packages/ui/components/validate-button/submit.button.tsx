4105a5133198d663e7141285633c3f16a79575c9
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:58:56 +0200

    creation du submit-button

---
+++ packages/ui/components/validate-button/submit.button.tsx
@@ -0,0 +1,18 @@
+import styles from "./validate.button.module.scss";
+import * as React from "react";
+import { MouseEventHandler } from "react";
+
+type Props = {
+    children: React.ReactNode,
+    onClick?: MouseEventHandler<HTMLButtonElement>
+}
+
+const SubmitButton: React.FC<Props> = ({ children, onClick }) => {
+    return (
+        <button onClick={onClick} type={"submit"} className={styles.validateButton}>
+            {children}
+        </button>
+    );
+};
+
+export default SubmitButton;
