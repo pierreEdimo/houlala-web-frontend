8f0d8ed48b088818a26fb17764760b784973ebfa
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:30:11 +0200

    refiner l'order.list.card

--- apps/houlala_sell/components/order-list/order.list.card.tsx
+++ apps/houlala_sell/components/order-list/order.list.card.tsx
@@ -6,63 +6,62 @@ import OrderItem from "../order-item/order.item";
 import Link from "next/link";
 
 type Props = {
-  url: string;
-  title: string;
-  locationId?: string
-  position?: string;
+    url: string;
+    title?: string;
+    locationId?: string
+    position?: string;
 }
 
 const OrderListCard: React.FC<Props> = ({ url, title, locationId, position }) => {
-  const { orders, error, isLoading } = useOrderList(url);
+    const { orders, error, isLoading } = useOrderList(url);
 
-  if (isLoading) {
-    return (
-      <div>
-        ...IsLoading
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div>
-        ...Error
-      </div>
-    );
-  }
-
-  return (
-    <>
-      {
-        orders?.length! < 1 ?
-          <div></div> :
-          <BorderedCard style={{ padding: "1rem" }}>
-            <div className={styles.cardHeader}>
-              <h3>{title}</h3>
-              {
-                position === "home" ?
-                  <Link href={`/orders/${locationId}`}>Voir tout</Link> :
-                  <div></div>
-              }
+    if (isLoading) {
+        return (
+            <div>
+                ...IsLoading
             </div>
-            <br />
-            <div style={{
-              display: "flex",
-              flexDirection: "column",
-              gap:"0.5rem"
-            }}>
-              {
-                orders?.map((order) => (
-                  <>
-                    <OrderItem key={order._id} order={order} />
-                  </>
-                ))
-              }
+        );
+    }
+
+    if (error) {
+        return (
+            <div>
+                ...Error
             </div>
-          </BorderedCard>
-      }
-    </>
-  );
+        );
+    }
+
+    return (
+        <>
+            {
+                orders?.length! < 1 ?
+                    <div></div> :
+                    <BorderedCard style={{ padding: "1rem", marginBottom: "1rem" }}>
+                        <div className={styles.cardHeader}>
+                            <h3>{title}</h3>
+                            {
+                                position === "home" ?
+                                    <Link href={`/orders/${locationId}`}>Voir tout</Link> :
+                                    <div></div>
+                            }
+                        </div>
+                        <br />
+                        <div style={{
+                            display: "flex",
+                            flexDirection: "column",
+                        }}>
+                            {
+                                orders?.map((order) => (
+                                    <>
+                                        <OrderItem key={order._id} order={order} />
+                                    </>
+                                ))
+                            }
+                        </div>
+                    </BorderedCard>
+            }
+        </>
+    );
 };
 
 export default OrderListCard;
