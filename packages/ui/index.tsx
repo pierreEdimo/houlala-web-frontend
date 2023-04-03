982a9398489f52b077ea1bd156d3a702eff60545
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 10:00:30 +0200

    les componants a importer

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -4,12 +4,36 @@ import Container from "./components/layout/container";
 import SideBar from "./components/side-bar/side.bar";
 import InnerLayout from "./components/layout/inner.layout";
 import Avatar from "./components/avatar/avatar";
+import BottomBar from "./components/bottom-bar/bottom.bar";
+import SubmitButton from "./components/validate-button/submit.button";
+import TabView from "./components/tab-view/tab.view";
+import TabHeader from "./components/tab-view/tab.header";
+import TabBody from "./components/tab-view/tab.body";
+import TabItem from "./components/tab-view/tab.item";
+import Modal from "./components/modal/modal";
+import RoundedButton from "./components/rounded-button/rounded.button";
+import OutlinedButton from "./components/outlined-button/outlined.button";
+import IconImage from "./components/icon-image/icon.image";
+import logOutIcon from "./public/log-out.png"; 
+import closeButton from "./public/close.png"; 
 
 export {
-  BorderedCard,
-  Row,
-  Container,
-  SideBar,
-  InnerLayout,
-  Avatar
+    BorderedCard,
+    Row,
+    Container,
+    SideBar,
+    InnerLayout,
+    Avatar,
+    BottomBar,
+    SubmitButton,
+    TabView,
+    TabHeader,
+    TabBody,
+    TabItem, 
+    Modal, 
+    RoundedButton, 
+    OutlinedButton, 
+    IconImage, 
+    logOutIcon, 
+    closeButton
 };
