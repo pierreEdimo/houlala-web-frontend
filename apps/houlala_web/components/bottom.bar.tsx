24e3008ab606adc18897c34bff3eaf9b7f337106
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:46:44 +0200

    suppimer L'iinutile

--- apps/houlala_web/components/bottom.bar.tsx
+++ apps/houlala_web/components/bottom.bar.tsx
@@ -19,30 +19,28 @@ const BottomBar = () => {
 
   return (
     <div className={styles.bottomBar}>
-      <NestedLayout>
-        <div className={styles.bottomBarContent}>
-          <Link href="/">
-            <div className={router.pathname === "/" ? styles.activeIcon : styles.iconContainer}>
-              <Image src={house} width={25} height={25} alt="house-image" />
-            </div>
-          </Link>
-          <Link href="/discover">
-            <div className={router.pathname === "/discover" ? styles.activeIcon : styles.iconContainer}>
-              <Image src={category} width={25} height={25} alt="category-image" />
-            </div>
-          </Link>
-          <Link href="/cart">
-            <div className={router.pathname === "/cart" ? styles.activeIcon : styles.iconContainer}>
-              <Image src={cart} width={25} height={25} alt="corbeille-image" />
-            </div>
-          </Link>
-          <div onClick={openModal}>
-            <div className={styles.iconContainer}>
-              <Image src={user} width={25} height={25} alt="user-image" />
-            </div>
+      <div className={styles.bottomBarContent}>
+        <Link href="/">
+          <div className={router.pathname === "/" ? styles.activeIcon : styles.iconContainer}>
+            <Image src={house} width={25} height={25} alt="house-image" />
+          </div>
+        </Link>
+        <Link href="/discover">
+          <div className={router.pathname === "/discover" ? styles.activeIcon : styles.iconContainer}>
+            <Image src={category} width={25} height={25} alt="category-image" />
+          </div>
+        </Link>
+        <Link href="/cart">
+          <div className={router.pathname === "/cart" ? styles.activeIcon : styles.iconContainer}>
+            <Image src={cart} width={25} height={25} alt="corbeille-image" />
+          </div>
+        </Link>
+        <div onClick={openModal}>
+          <div className={styles.iconContainer}>
+            <Image src={user} width={25} height={25} alt="user-image" />
           </div>
         </div>
-      </NestedLayout>
+      </div>
     </div>
   );
 };
