d4caddda6163fc62bc7f8bf92d99c703cac44de4
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-03 09:29:51 +0200

    le style d'order.item ameliore

--- apps/houlala_sell/components/order-item/order.item.tsx
+++ apps/houlala_sell/components/order-item/order.item.tsx
@@ -6,59 +6,35 @@ import IconImage from "../icon-image/icon.image";
 import calendar from "../../public/images/calendar.png";
 import moneyBag from "../../public/images/money-bag.png";
 import StatusButton from "./status.button";
+import { useRouter } from "next/router";
 
 type Props = {
-  order: Order
+    order: Order
 }
 
 const OrderItem: React.FC<Props> = ({ order }) => {
-  return (
-    <div className={styles.orderContainer}>
-      <div style={{
-        width: "270px",
-        display: "flex",
-        gap: "1rem",
-        alignItems: "center"
-      }}>
-        <IconImage icon={successOrder} width={17} height={17} />
-        <div>
-          <p style={{
-            width: "190px",
-            overflow: "hidden",
-            whiteSpace: "nowrap",
-            textOverflow: "ellipsis"
-          }}>{order.userInformation.lastName} {order.userInformation.firstName}
-          </p>
+    const router = useRouter();
+
+    return (
+        <div onClick={() => router.push(`/order-detail/${order._id}`)} className={styles.orderContainer}>
+            <div className={styles.orderContainerItem}>
+                <IconImage icon={successOrder} width={15} height={15} />
+                <p id={styles.nameId}>{order.userInformation.lastName} {order.userInformation.firstName} </p>
+            </div>
+            <div className={styles.orderContainerItem} >
+                <IconImage icon={calendar} width={15} height={15} />
+                <p>{new Date(order.createdAt).toLocaleDateString()}</p>
+            </div>
+            <div className={styles.orderContainerItem}>
+                <StatusButton status={order.status} />
+            </div>
+            <div id={styles.priceContainer} className={styles.orderContainerItem}>
+                <IconImage icon={moneyBag} width={15} height={15} />
+                <p id={styles.nameId}>{order.totalPrice} XAF</p>
+            </div
+            >
         </div>
-      </div>
-      <div style={{
-        width: "90px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}>
-        <IconImage icon={calendar} width={17} height={17} />
-        <p>{new Date(order.createdAt).toLocaleDateString()}</p>
-      </div>
-      <div style={{
-        width: "90px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}>
-        <StatusButton status={order.status} />
-      </div>
-      <div style={{
-        width: "90px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}>
-        <IconImage icon={moneyBag} width={17} height={17} />
-        <p>{order.totalPrice} FCFA</p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default OrderItem;
