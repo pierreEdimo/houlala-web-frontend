42f48b370068adfba3b0c08acbe39845cc18a4e9
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-05 12:14:51 +0200

    ajouter la methode pour envoyer le help email

--- apps/houlala_sell/components/help-modal/help.modal.tsx
+++ apps/houlala_sell/components/help-modal/help.modal.tsx
@@ -1,17 +1,58 @@
 import { BorderedCard, Modal, OutlinedButton, SubmitButton } from "ui"
 import { ModalIsEnum } from "../../types/modal.ids";
 import styles from "./help.modal.module.scss"
+import { Help } from "../../types/help";
+import { useRecoilState } from "recoil";
+import { useState } from "react";
+import EmailService from "../../service/email.service";
 
 const HelpModal = () => {
+    const [message, setErrorMessage] = useState<string>()
+    const emailService = new EmailService();
+    const EMAIL_URL = process.env.NEXT_PUBLIC_EMAIL_URL;
     const closeModal = () => {
         const modal = document.getElementById(ModalIsEnum.help);
-        modal!.style.display = "none";  
+        modal!.style.display = "none";
+    }
+
+    const sendHelpEmail = async (event: any) => {
+        event.preventDefault();
+        const data: Help = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            phoneNumber: event.target.phoneNumber.value,
+            subject: event.target.topic.value,
+            message: event.target.issue.value,
+            to: "pierredimo@live.com"
+        }
+        const response = await emailService.sendEmail(`${EMAIL_URL}/help`, data);
+        
+        if (response.status === 200) {
+            event.target.reset();
+            closeModal();
+        }
+
+        if (response.status !== 200) {
+            setErrorMessage("Une erreur est survenue pendant l'envoie de votre E-mail" +
+                "Veuillez reessayer plutard. Si le probleme persiste, contactez nous au 0023799917462");
+            return;
+        }
+
     }
 
     return (
         <Modal modalId={ModalIsEnum.help}>
             <BorderedCard style={{ padding: "1rem", background: "white" }}>
-                <form className={styles.helpForm}>
+                <form onSubmit={sendHelpEmail} className={styles.helpForm}>
+                    <div>
+                        {
+                            message ?
+                                <div className={styles.errorMessage}>
+                                    {message}
+                                </div> :
+                                <div></div>
+                        }
+                    </div>
                     <div>
                         <h3>Avez besoin d'aide?</h3>
                         <p style={{ fontSize: "14px", color: "grey" }}>Remplissez le formulaire de contact</p>
@@ -42,7 +83,9 @@ const HelpModal = () => {
                     />
                     <textarea
                         placeholder={'Decrivez votre probleme'}
-                        name={"issue"}>
+                        name={"issue"}
+                        required
+                    >
                     </textarea>
                     <div className={styles.helpModalFooter}>
                         <OutlinedButton onClick={closeModal}>
