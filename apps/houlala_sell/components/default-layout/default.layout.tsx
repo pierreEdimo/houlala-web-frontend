e3be693b5eac718f7626c9a82744b66b2b0bce89
pierre Edimo <pedimonkoe@yahoo.com>
2023-04-05 12:14:15 +0200

    creer le state pour initialiser le UserId

--- apps/houlala_sell/components/default-layout/default.layout.tsx
+++ apps/houlala_sell/components/default-layout/default.layout.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from "recoil";
 import AuthState from "../../state/auth.state";
 import { UserToken } from "../../types/user.token";
 import Login from "../login";
+import { UserIdState } from "../../state/user.id.state";
 
 type Props = {
     children: React.ReactNode
@@ -11,11 +12,13 @@ type Props = {
 const DefaultLayout: React.FC<Props> = ({ children }) => {
     const [isLoggedIn, setIsLoggin] = useRecoilState(AuthState);
     const [loading, setIsLoading] = React.useState(true);
+    const [userId, setUserId] = useRecoilState(UserIdState); 
 
     React.useEffect(() => {
         setIsLoading(false);
         const userToken: UserToken = JSON.parse(localStorage!.getItem('userToken')!); 
         if(userToken){
+            setUserId(userToken.userId!); 
             setIsLoggin(true); 
         }
     }, []);
