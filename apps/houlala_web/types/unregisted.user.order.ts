9450b257ebe703d69158b64fe91c9fd1aa23e57b
pierre Edimo <pedimonkoe@yahoo.com>
2023-09-02 11:03:34 +0200

    remplacer tsx en ts

--- apps/houlala_web/types/unregisted.user.order.ts
+++ apps/houlala_web/types/unregisted.user.order.ts
@@ -2,7 +2,7 @@ import {CartItem} from "./cart.item";
 import {UserInformation} from "./user.information";
 
 export type UnregistedUserOrder = {
-    locationId: string;
+    locationUniqueId: string;
     userInformation: UserInformation;
     cartItems: Array<CartItem>;
 }
