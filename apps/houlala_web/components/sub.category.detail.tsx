d8011964855c10056398a7b14823d848afeceb38
pierre Edimo <pedimonkoe@yahoo.com>
2023-09-02 10:48:23 +0200

    mettre l'Url du subCategoryDetail a jour

--- apps/houlala_web/components/sub.category.detail.tsx
+++ apps/houlala_web/components/sub.category.detail.tsx
@@ -1,32 +1,33 @@
 import React from "react";
 import ProductGrid from "./product.grid";
-import {useSubCategory} from "../swrHooks/sub.category.hooks";
+import { useSubCategory } from "../swrHooks/sub.category.hooks";
 import BackButton from "./back.button";
+import { HoulalaSpinner } from "ui";
 
 type SubCategoryDetailProps = {
-    categoryId: string;
-}
+  categoryId: number;
+};
 
-const SubCategoryDetail: React.FC<SubCategoryDetailProps> = ({categoryId}: { categoryId: string }) => {
-    const CATEGORY_URL = process.env.NEXT_PUBLIC_SUBCATEGORY_URL;
-    const {
-        category,
-        isLoading,
-        isError
-    } = useSubCategory(`https://api.houlala.store/marketplace/sub-categories/${categoryId}`);
-    const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
+const SubCategoryDetail: React.FC<SubCategoryDetailProps> = ({
+  categoryId,
+}) => {
+  const CATEGORY_URL = process.env.NEXT_PUBLIC_SUBCATEGORY_URL;
+  const { category, isLoading, isError } = useSubCategory(
+    `${CATEGORY_URL}/${categoryId}`
+  );
+  const PRODUCT_URL = process.env.NEXT_PUBLIC_PRODUCT_URL;
 
-    if (isLoading) return <div>...Loading</div>
-    if (isError) return <div>{CATEGORY_URL}</div>
+  if (isLoading) return <div><HoulalaSpinner/></div>;
+  if (isError) return <div>....Error</div>;
 
-    return (
-        <>
-            <div>
-                <BackButton title={category?.label!}/>
-                <ProductGrid url={`${PRODUCT_URL}/type/${categoryId}?limit=0`}/>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <BackButton title={category?.label!} />
+        <ProductGrid url={`${PRODUCT_URL}/sub-categories/${categoryId}/size/0`} />
+      </div>
+    </>
+  );
+};
 
 export default SubCategoryDetail;
