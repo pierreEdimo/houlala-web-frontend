f12ab0c1adcbbc455a6d8d03f6cfd4319276ca2e
pierre Edimo <pedimonkoe@yahoo.com>
2023-09-02 10:55:43 +0200

    ajouter persistAtom

--- apps/houlala_web/state/user.email.tsx
+++ apps/houlala_web/state/user.email.tsx
@@ -1,6 +1,9 @@
 import {atom} from "recoil";
+import { recoilPersist } from "recoil-persist";
 
+const {persistAtom} = recoilPersist();
 export const UserEmailState = atom({
     key: "userEmail",
-    default: "" as string
+    default: "" as string,
+    effects_UNSTABLE: [persistAtom]
 });
