ed8fa3cc9e9cff3f277cf9543f1dfeb9ec1a3c3e
pierre Edimo <pedimonkoe@yahoo.com>
2023-09-02 10:54:30 +0200

    ajouter le flexbox

--- apps/houlala_web/pages/index.tsx
+++ apps/houlala_web/pages/index.tsx
@@ -1,26 +1,23 @@
-import type {NextPage} from 'next'
-import {HomeCategoryList} from "../components/home.category.list";
-import {HomeProductList} from "../components/home.product.list";
-import {HomeLocationList} from "../components/home.location.list";
-import {NestedLayout} from "../components/nested.layout";
-
+import type { NextPage } from "next";
+import { HomeCategoryList } from "../components/home.category.list";
+import { HomeProductList } from "../components/home.product.list";
+import { HomeLocationList } from "../components/home.location.list";
+import { NestedLayout } from "../components/nested.layout";
 
 const Home: NextPage = () => {
-    return (
-        <div>
-            <main>
-                <NestedLayout>
-                    <HomeCategoryList/>
-                    <br/>
-                    <br/>
-                    <HomeProductList/>
-                    <br/>
-                    <br/>
-                    <HomeLocationList/>
-                </NestedLayout>
-            </main>
-        </div>
-    )
-}
+  return (
+    <div>
+      <main>
+        <NestedLayout>
+          <div style={{display: "flex", flexDirection: "column", gap: "50px"}}>
+            <HomeCategoryList />
+            <HomeProductList />
+            <HomeLocationList />
+          </div>
+        </NestedLayout>
+      </main>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
