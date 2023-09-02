78877622e68df667ec40f881320efb30907572fc
pierre Edimo <pedimonkoe@yahoo.com>
2023-09-02 10:37:06 +0200

    refactoring du localDb

--- apps/houlala_web/localDb/database.config.ts
+++ apps/houlala_web/localDb/database.config.ts
@@ -3,9 +3,10 @@ import Dexie from "dexie";
 const database = new Dexie("houlalaDb");
 
 database.version(1).stores({
-    offlineOrders: '++id,locationId,locationName,totalQuantity,totalPrice,payMentMode'
+  offlineOrders:
+    "++id,locationUniqueId,locationName,totalQuantity,totalPrice,payMentMode",
 });
 
-export const OfflineOrderTable = database.table('offlineOrders');
+export const OfflineOrderTable = database.table("offlineOrders");
 
 export default database;
